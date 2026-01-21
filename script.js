48468b8a0e614858ae8cc68ce2520a5661b83cd7
João Batista <joaobatista231516@gmail.com>
2026-01-21 01:57:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ async function falarComIA() {
     `;
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
@@ -365,4 +365,5 @@ function endQuiz() {
             </div>
         `;
     }
+
 }
